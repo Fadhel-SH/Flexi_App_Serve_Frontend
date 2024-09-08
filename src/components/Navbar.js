9c332f070f8589d2e38447ac240290bd9510e1dc
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-09-09 02:56:40 +0300

    Add Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>My App</h1>
+            <ul>
+                <li>Login</li>
+                <li>Register</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
