e26ced961ee7e6654d105ed0cf541ef7e5cd62fd
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-09-09 03:15:16 +0300

    Implement login and register logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
@@ -12,10 +13,13 @@ function App() {
       <Router>
           <div className="App">
               <Navbar />
-              <Switch>
+              <Routes>
                   <Route path="/login" component={Login} />
                   <Route path="/register" component={Register} />
-              </Switch>
+                  <Route path="/about" component={About} />
+                  <Route path="/contact" component={Contact} />
+                  <Route path="/" component={Home} />
+                  </Routes>
           </div>
       </Router>
   );
